3c972d081e914fcb4199e12446fa41a905614301
Joel Rivera <rivera@joel.mx>
2018-09-30 19:23:41 -0500

    Make the mkBuild function, truly overridable. Used to have some issues with the initial attributes verification.

--- builders/base/default.nix
+++ builders/base/default.nix
@@ -1,8 +1,5 @@
-{ pkgs, utils }:
-rec {
- inherit pkgs; # reference for the base nixpkgs
- inherit (utils) bigErrorMsg;
-
+{ pkgs, lib, utils }:
+let
  mkBuild =
   { name ? "unknown"
    , script ? null
@@ -15,66 +12,73 @@ rec {
    , directAttrs ? {}
    , passThru ? {}
    , meta ? {} }:
-   assert (script == null && scriptPath == null) ->
-      abort (bigErrorMsg "Missing 'script' or 'scriptPath' parameter.");
-   assert (script != null && scriptPath != null) ->
-      abort (bigErrorMsg "You have to define either 'script' or 'scriptPath', not both.");
    let
-     inherit(pkgs) system bash coreutils moreutils gnused;
-     inherit(pkgs.lib)
-       optional optionalAttrs concatStringsSep makeOverridable
-       mapAttrs' nameValuePair strings addMetaAttrs;
-     inherit(builtins) currentTime;
+    inherit (pkgs) bash system;
+    inherit(pkgs.lib)
+      optionalAttrs concatStringsSep  mapAttrs' nameValuePair strings;
+    coreAttributes = {
+      inherit system;
+      inherit allowedSystemCmds;
+      name = (if namePrefix == null
+              then name
+              else "${ namePrefix }-${name}");
+      builder = "${ bash }/bin/bash";
+      passAsFile = [ "setupEnv" "script"];
+      args = [ ./base-builder.sh ];
+      stdenvUtilsPath = ./stdenv-utils.sh;
+      inputPath =
+        let
+          binPaths = map (pkg: "${ pkg }/bin") buildInputs;
+        in
+          concatStringsSep ":" binPaths;
+      setupEnv = with pkgs;
+      ''
+         PATH="${ bash }/bin/:${ coreutils }/bin/:${ moreutils }/bin:${ gnused }/bin:$inputPath"
+         if [[ -n $allowedSystemCmds ]]; then
+             _TEMP_PATH="$(pwd)/.build_path"
+             mkdir $_TEMP_PATH
+             set +e
+             for cmd in $allowedSystemCmds; do
+                 full_cmd=$(readlink -e $cmd)
+                 if (( $? != 0 )); then
+                     echo "The command '$cmd' is not present in the system. Ignoring" > /dev/stderr
+                 else
+                     base_name=$(basename $cmd)
+                     ln -s $full_cmd $_TEMP_PATH/$base_name
+                 fi
+             done
+             set -e
+             PATH="$PATH:$_TEMP_PATH"
+         fi
+         export PATH
+      '';
+    };
 
-     coreAttributes = {
-       inherit system allowedSystemCmds;
-       name = (if namePrefix == null
-               then name
-               else "${ namePrefix }-${name}");
-       builder = "${ bash }/bin/bash";
-       args = [ ./base-builder.sh ];
-       stdenvUtilsPath = ./stdenv-utils.sh;
-       passAsFile = [ "setupEnv" ] ++ optional (script != null) "script";
-       inputPath =
-         let
-           binPaths = map (pkg: "${ pkg }/bin") buildInputs;
-         in
-           concatStringsSep ":" binPaths;
-       setupEnv = ''
-          PATH="${ bash }/bin/:${ coreutils }/bin/:${ moreutils }/bin:${ gnused }/bin:$inputPath"
-          if [[ -n $allowedSystemCmds ]]; then
-              _TEMP_PATH="$(pwd)/.build_path"
-              mkdir $_TEMP_PATH
-              set +e
-              for cmd in $allowedSystemCmds; do
-                  full_cmd=$(readlink -e $cmd)
-                  if (( $? != 0 )); then
-                      echo "The command '$cmd' is not present in the system. Ignoring" > /dev/stderr
-                  else
-                      base_name=$(basename $cmd)
-                      ln -s $full_cmd $_TEMP_PATH/$base_name
-                  fi
-              done
-              set -e
-              PATH="$PATH:$_TEMP_PATH"
-          fi
-          export PATH
-       '';
-     };
-     environmentVariables =
-       mapAttrs' (name: value:
-                   nameValuePair "ENV_${strings.toUpper name}" value)
-                 envVars;
-    plainDerivation = derivation (
+    # transform the explicitly passed attribute set to be used as environment variable
+    # from "foo" to "ENV_FOO"
+    environmentVariables =
+      mapAttrs' (name: value:
+                  nameValuePair "ENV_${strings.toUpper name}" value)
+                envVars;
+
+    derivationArgs = (
        coreAttributes
        // environmentVariables
+       // directAttrs
        # make sure we modify the build inputs so we can guarantee that the
        # build is going to be executed, we are not looking for purity here.
-       // directAttrs
        // optionalAttrs (ensureRebuild == true) { variant = builtins.currentTime; }
-       // optionalAttrs (script != null) { inherit script; }
-       // optionalAttrs (scriptPath != null) { inherit scriptPath; }
-    );
+       # give preference to the explicit 'script' parameter, otherwise try to use
+       # scriptPath, if none is defined throw an error.
+       // (if (script != null) then  { inherit script; }
+           else
+             assert (scriptPath == null) ->
+               throw (utils.bigErrorMsg "Missing 'script' or 'scriptPath' parameter.");
+             # We are been explicitly wasteful by reading the input script when the
+             # user provides a path, small trade-off to make it consistent on do we
+             # create the scriptPath attribute (by using passAsFile)
+             { script = builtins.readFile scriptPath; }));
    in
-    plainDerivation // passThru // { inherit meta; };
-}
+    (derivation derivationArgs) // passThru // { inherit meta; };
+in
+{ mkBuild = lib.makeOverridable mkBuild;}
--- builders/default.nix
+++ builders/default.nix
@@ -1,6 +1,7 @@
-{ callPackage }:
+{ pkgs, callPackage }:
 let
   base = callPackage ./base { };
   python = callPackage ./python { };
 in
- base // python
+ # add a reference for mere convenience for pkgs
+ { inherit pkgs; } // base // python
